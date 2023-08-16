5cdf98163713008ea6153c91cbcccceac28f03a5
Pavel Antropov <p.m.antropov@gmail.com>
2023-08-16 21:40:50 +0400

    Create validators

---
+++ src/HH.MindBox.Shapes.Validation/CircleValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using HH.MindBox.Shapes.Domain.Entities;
+
+namespace HH.MindBox.Shapes.Validation;
+
+public class CircleValidator : AbstractValidator<Circle>
+{
+	public CircleValidator()
+	{
+		RuleFor(s => s.Radius).GreaterThan(0);
+	}
+}
---
+++ src/HH.MindBox.Shapes.Validation/TriangleValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using HH.MindBox.Shapes.Domain.Entities;
+
+namespace HH.MindBox.Shapes.Validation;
+
+public class TriangleValidator : AbstractValidator<Triangle>
+{
+	public TriangleValidator()
+	{
+		RuleFor(s => s.SideA).GreaterThan(0);
+		RuleFor(s => s.SideB).GreaterThan(0);
+		RuleFor(s => s.SideC).GreaterThan(0);
+
+		RuleFor(s => s.SideA + s.SideB).GreaterThan(s => s.SideC);
+		RuleFor(s => s.SideA + s.SideC).GreaterThan(s => s.SideB);
+		RuleFor(s => s.SideB + s.SideC).GreaterThan(s => s.SideA);
+	}
+}
